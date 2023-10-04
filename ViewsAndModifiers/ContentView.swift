ce7e15f052b99c22ecf16a924cdc769e5d5f9d45
leotn03 <leo.tn03@icloud.com>
2023-10-04 09:38:34 -0500

    Initial Commit

---
+++ ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Leo Torres Neyra on 4/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
